280185edcfd8d87febf743806e142fa6f4c7dec1
Makofour <makofour4@gmail.com>
2024-09-28 15:31:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎮 W GRZE Eternal RP"];
+const statusMessages = ["Eternal RP on top "];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
